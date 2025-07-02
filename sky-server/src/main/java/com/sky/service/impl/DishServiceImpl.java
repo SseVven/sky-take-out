2d6a02879b0e0db1c45a72222c53723e3a8b91f3
SseVven <1586850294@qq.com>
2025-07-02 17:35:39 +0800

    day03: 菜品分页查询：分页查询 pagehelper

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,11 +1,18 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Category;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
+import com.sky.mapper.CategoryMapper;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +30,9 @@ public class DishServiceImpl implements DishService {
 
     @Autowired
     private DishFlavorMapper dishFlavorMapper;
+
+    @Autowired
+    private CategoryMapper  categoryMapper;
 
     /**
      * 新增菜品和对应的口味
@@ -47,4 +57,23 @@ public class DishServiceImpl implements DishService {
             dishFlavorMapper.insertBatch(flavors);
         }
     }
+
+    /**
+     * 分页查询菜品
+     * @param dishPageQueryDTO
+     * @return
+     */
+    public PageResult page(DishPageQueryDTO dishPageQueryDTO) {
+        PageHelper.startPage(dishPageQueryDTO.getPage(), dishPageQueryDTO.getPageSize());
+
+        Page<DishVO> pages = dishMapper.pageQuery(dishPageQueryDTO);
+        List<DishVO> dishes = pages.getResult();
+        dishes.forEach(dish -> {
+            Category category = categoryMapper.getById(dish.getCategoryId());
+            dish.setCategoryName(category.getName());
+        });
+
+        return new PageResult(pages.getTotal(), dishes);
+    }
+
 }
