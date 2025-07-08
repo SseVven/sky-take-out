c2fc71ad6593a484f7695c55565d241d7106d083
SseVven <1586850294@qq.com>
2025-07-08 16:05:08 +0800

    day07: 购物车业务

---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,33 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
+
+public interface ShoppingCartService {
+
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    /**
+     * 展示购物车
+     * @return
+     */
+    List<ShoppingCart> list();
+
+    /**
+     * 清空购物车
+     */
+    void clear();
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+}
